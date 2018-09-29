837425748a9de4e2aefbaddaa22cdc29f5b0235f
sushilraut20 <sushilraut20@gmail.com>
2018-09-29 14:14:31 -0700

    Added comment for occurrences

--- com/BasicDataStructureProblems.py
+++ com/BasicDataStructureProblems.py
@@ -32,6 +32,7 @@ while True:
     position=input_string.find(input_substring,position+1,len(input_string))
     # string.find(substring, start_index, end_index)
     # 'find' will return the position of occurrence of a substring in given String, will return -1 if the substring is not found
+    # the start_index is updated for every iteration
     if position==-1:
         break
     occurence_list.append(position)
