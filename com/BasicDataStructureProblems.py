ebe7adb95df798ae79a1179499c215d2400084e8
sushilraut20 <sushilraut20@gmail.com>
2018-10-03 12:15:58 -0700

    added Input: A2B4C3, Output: AABBBBCCC logic

--- com/BasicDataStructureProblems.py
+++ com/BasicDataStructureProblems.py
@@ -62,3 +62,22 @@ for i in sorted(numeric_string):
 
 print("sorted alphanumeric string: ", sorted_alphanumeric_string)
 
+
+#-----------Input: A2B4C3, Output: AABBBBCCC ---------------------------------------------------
+#Note: The integers must be less than 10 and greater than 0
+
+input_string=input("Enter an alphanumeric String: ")
+alphabets_list=[]
+numeric_list=[]
+output_string=''
+
+for str in input_string:
+    if str.isalpha():
+        alphabets_list.append(str)
+    elif str.isdigit():
+        numeric_list.append(int(str))
+
+for i in range(len(alphabets_list)):
+    output_string+=alphabets_list[i]*numeric_list[i]
+
+print("Output string: ", output_string)
