9d4bd52484a0d5d27245ffffbb5ce203f0ecde70
sushilraut20 <sushilraut20@gmail.com>
2018-10-05 11:29:58 -0700

    Added 'Return indices of the two numbers such that they add up to a specific target'

--- com/List_DataStructureProblems.py
+++ com/List_DataStructureProblems.py
@@ -1,4 +1,5 @@
-""""Given a list of integers, find an index of an element, where
+""""
+Given a list of integers, find an index of an element, where
 sum of elements to the right is exactly same as that of sum of elements to the right
  e.g. input_list=[1,9,3,0,10]
        output= 2
@@ -30,3 +31,37 @@ while 0<=index<len(input_list):
     else:
         index=index+1
 else: print("There is no any element in List where the given condition gets satisfied")
+
+#----------------------------------------LEETCODE Problem--------------------------------------------------------------------
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+nums=[-1,-2,-3,-6,-8]
+target=-9
+first_number=0
+index_list=[]
+
+
+for i in nums:
+    first_number=i
+
+    if (target-first_number) not in nums[nums.index(i)+1:]:
+        continue
+    else:
+        second_number=nums.index(target-first_number,nums.index(first_number)+1)
+        index_list.append(("First number index",nums.index(first_number)))
+        index_list.append(("Second Number index",second_number))
+
+    break
+
+print("Required list of indexes: ", index_list)
