bd5eb6c97949d60852c05b60f20a3f6a603382ad
sushilraut20 <sushilraut20@gmail.com>
2018-10-11 14:03:42 -0700

    Added unique and common elements from 2 Lists

--- com/List_DataStructureProblems.py
+++ com/List_DataStructureProblems.py
@@ -65,3 +65,33 @@ for i in nums:
     break
 
 print("Required list of indexes: ", index_list)
+
+#----------------------------------------List of unique elements from 2 lists---------------------------------------------------
+list1=[1,3,6,2,5]
+list2=[4,3,5,9,8,8]
+unique_element_list=[]
+
+for i in list1:
+    if i not in list2 and i not in unique_element_list:
+        unique_element_list.append(i)
+
+for i in list2:
+    if i not in list1 and i not in unique_element_list:
+        unique_element_list.append(i)
+
+print(unique_element_list)
+
+#----------------------------------------List of common elements from 2 lists---------------------------------------------------
+list1=[1,3,6,2,5]
+list2=[4,3,5,9,8,8]
+common_element_list=[]
+
+for i in list1:
+    if i in list2 and i not in common_element_list:
+        common_element_list.append(i)
+
+for i in list2:
+    if i in list1 and i not in common_element_list:
+        common_element_list.append(i)
+
+print(common_element_list)
