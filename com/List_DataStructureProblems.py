5a94f86cb1196a40686fb8bd0a53c7ae9ec3c287
sushilraut20 <sushilraut20@gmail.com>
2018-10-11 14:16:42 -0700

    Added most popular element in list

--- com/List_DataStructureProblems.py
+++ com/List_DataStructureProblems.py
@@ -95,3 +95,18 @@ for i in list2:
         common_element_list.append(i)
 
 print(common_element_list)
+
+#----------------------------------------Most popular element in a List---------------------------------------------------------
+
+input_list=[1,1,2,3,4,5,5,5]
+unique_elements_list=[]
+occurence_count_list=[]
+
+for i in input_list:
+    if i not in unique_elements_list:
+        unique_elements_list.append(i)
+
+for i in unique_elements_list:
+    unique_elements_list.append(input_list.count(i))
+
+print("The most popular element in given list is: ",unique_elements_list[occurence_count_list.index(max(occurence_count_list))])
