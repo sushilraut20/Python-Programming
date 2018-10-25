e869ee0272e00ee8da9314cb14fccb97bb0bb819
Sushil Namdeo Raut <sraut@sdsu.edu>
2018-10-24 23:59:58 -0700

    added 'Counting occurrences of vowels in a String using dictionary'

--- com/String_DataStructureProblems.py
+++ com/String_DataStructureProblems.py
@@ -170,5 +170,18 @@ for alphabet in input_string:
 for key,value in sorted(occurrences_dict.items()):
     print(key,"------->",value)
 
+#-----------Counting occurrences of vowels in a String using dictionary -----------------------------------
+
+input_string=input("Enter a String: ")
+vowel_occurrences_dict={'a':0,'e':0,'i':0,'o':0,'u':0}
+
+for alphabet in input_string:
+    if alphabet.lower() in vowel_occurrences_dict:# it will check the keys in vowel_occurrences_dict
+        vowel_occurrences_dict[alphabet.lower()]=vowel_occurrences_dict.get(alphabet.lower())+1
+#occurrences_dict.get(alphabet) will fetch the value for that alphabet(key)
+
+
+for key,value in vowel_occurrences_dict.items():
+    print(key,"------->",value)
 
 
