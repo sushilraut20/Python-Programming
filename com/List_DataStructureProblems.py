f0eda52581114b271948de09b4c579790d032fb6
Sushil Namdeo Raut <sraut@sdsu.edu>
2018-10-24 19:38:39 -0700

    Added 'Word count in paragraph consisting multiple lines'

--- com/List_DataStructureProblems.py
+++ com/List_DataStructureProblems.py
@@ -110,3 +110,19 @@ for i in unique_elements_list:
     unique_elements_list.append(input_list.count(i))
 
 print("The most popular element in given list is: ",unique_elements_list[occurence_count_list.index(max(occurence_count_list))])
+
+#----------------------------------------Word count in paragraph consisting multiple lines----------------------------------------
+
+paragraph="This is San Diego. It is an amazing place to live. San Diego is on west coast".lower().split(".")
+
+paragraph_words_list=[]
+unique_words_list=[]
+
+for sentence in paragraph:
+    for i in sentence.strip().split():
+        paragraph_words_list.append(i)
+        if i not in unique_words_list:
+            unique_words_list.append(i)
+
+for word in unique_words_list:
+    print("'",word,"'", " is present in paragraph for ","'",paragraph_words_list.count(word),"'", "times")
